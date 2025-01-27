1221650cac537695ddacbc8d2769d1838a8d9f7e
unknown <sundarg1502@gmail.com>
2025-01-27 15:37:49 +0530

    Feat : Added js

--- Tutorial/Day7/js/basic.js
+++ Tutorial/Day7/js/basic.js
@@ -48,16 +48,88 @@ function scope() {
 
 // console.log("end");
 
+// Prmoise In async
+// console.log("Start")
+// const fetchData = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve("Data Fetched Succceccfully");  
+//     },2000);
+// });
+// fetchData.then((data)=>{
+//     console.log(data)
+// })
+// .catch((error)=>{
+//     console.log("Error")
+// })
 
-console.log("Start")
-const fetchData = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve("Data Fetched Succceccfully");
-    },2000);
-});
-fetchData.then((data)=>{
-    console.log(data)
-})
-.catch((error)=>{
-    console.log("Error")
-})
+//Closure
+/*
+A Closure is a function that has access to its outer functions variable even after the outer function has returned 
+*/
+function outerFunction(outerVar){
+    return function innerFunction(innerVar){
+        console.log(`Outer Value : ${outerVar}`)
+        console.log(`Inner Value : ${innerVar}`)
+        return true
+    }
+}
+// const closureFunc = outerFunction("Outer Value");
+// console.log(closureFunc("Inner Value"));
+
+// console.log(innerFunction("Sperate Value"))
+
+// Currying
+
+// It is the process of transforming a function with multipile arguments with a seires of function, 
+// each taking a  single argument
+
+// function multiply(a){
+//     a++
+//     console.log(a);
+//     return function(b){
+//         b++;
+//         console.log(b)
+//         return function(c){
+//             return a*b*c;
+//         };
+//     };
+// };
+// const multi = multiply(10);
+// console.log(multi)
+
+// Prototype
+//Example 1
+// function Person(name,age){
+//     this.name = name
+//     this.age = age
+// }
+// const alice = new Person("Alice",40) ;
+// console.log(alice.age)
+// const alice2 = new Person("Alice @")
+// console.log(alice2.age)
+
+// Example 2
+// function Person(name,age){
+//     this.name = name
+//     this.age = age
+// }
+// Person.prototype.greet = function(){
+//     console.log(`Welcome ${this.name}!!! ${this.age}`)
+// }
+// const alice = new Person("Alice",40) ;
+// alice.greet()
+// console.log(alice)
+// const alice2 = new Person("Alice @")
+// console.log(alice2.age)
+
+//  Nested Function
+function outerFunction(){
+    const outerVariable = "Outer"
+    function innnerFunction(){
+        console.log("Accessing innerfunction "+outerVariable)
+    }
+    console.log("Accessing innerfunction "+outerVariable)
+
+    innnerFunction()
+}
+outerFunction()
