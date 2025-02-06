c04bc51f46cf13519559b50c194e538d0a06210e
sundarg1502 <sundarg1502@gmail.com>
2025-02-06 23:50:38 +0530

    Feat Learnt inverted curves in css

---
+++ Projects/Food-King-Project/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <h3>Crispy, Every Bite Taste</h3>
+        <h1>Awsome Fried Chicken</h1>
+        
+    </div>
+  )
+}
+
+export default Hero
